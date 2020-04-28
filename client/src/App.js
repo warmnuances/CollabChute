39a8d218cbaee317317859d7b34f19d6ea123c1c
hlynn <hlynn@student.unimelb.edu.au>
2020-04-28 19:49:49 +1000

    Changed Frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import Landing from './Pages/Landing';
+import SignIn from './Pages/SignIn';
+import SignUp from './Pages/SignUp';
+import WorkSpace from './Pages/WorkSpace';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path="/" component={Landing}/>
+        <Route exact path="/signin" component={SignIn}/>
+        <Route exact path="/signup" component={SignUp}/>
+        <Route exact path="/workspace/:projid" component={WorkSpace}/>
+      </Switch>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createBrowserHistory } from 'history';
+import store from './redux/store';
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Router history={createBrowserHistory()}>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
